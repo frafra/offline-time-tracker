fe917a8f8ed229c76009379971238c64e6daf5b3
Francesco Frassinelli <fraph24@gmail.com>
2017-08-29 14:23:38 +0200

    Initial import

---
+++ tracker.js
@@ -0,0 +1,43 @@
+let display = document.getElementById('display');
+
+let start;
+let interval;
+let counter = 0;
+
+function millisecondsToHHMMSS(counter) {
+  let seconds = Math.floor(counter/1000);
+  let time = [
+    Math.floor(seconds/60/60),
+    Math.floor(seconds/60%60),
+    Math.floor(seconds%60)
+  ];
+  time.forEach((element, index, array) => {
+    if (element<10) array[index] = '0'+element;
+  });
+  return time.join(':');
+}
+
+function updateTimer() {
+  counter = +(new Date())-start;
+  let timestamp = millisecondsToHHMMSS(counter);
+  display.innerText = timestamp;
+}
+
+function startCounting() {
+  if (interval) return;
+  start = +(new Date())-counter;
+  interval = setInterval(updateTimer, 1000);
+}
+
+function stopCounting() {
+  clearInterval(interval);
+  interval = false;
+}
+
+window.addEventListener('load', _ => {
+  let display = document.getElementById('display');
+  if (!navigator.onLine) startCounting();
+  window.addEventListener('online', stopCounting);
+  window.addEventListener('offline', startCounting);
+});
+
